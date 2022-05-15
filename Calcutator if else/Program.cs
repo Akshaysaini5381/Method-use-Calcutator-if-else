ade1bd69eeaf0cfafc791cd417762b0591a8cdc8
Akshaysaini5381 <akshaysaini5381@gmail.com>
2022-05-15 23:04:24 +0530

    Add project files.

---
+++ Calcutator if else/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calcutator_if_else
+{
+    class Program
+    {
+        static void print(int num, int num1, string choice)
+        {
+            if (choice == "+")
+                Console.WriteLine(num + num1);
+            else if (choice == "-")
+                Console.WriteLine(num - num1);
+            else if (choice == "*")
+                Console.WriteLine(num * num1);
+            else if (choice == "/")
+                Console.WriteLine(num / num1);
+            else
+                Console.WriteLine("Not found result");
+        }
+        static void Main(string[] args)
+        {
+            Console.Write("Enter the number:");
+            int a = int.Parse(Console.ReadLine());
+            Console.Write("Enter the number:");
+            int b = int.Parse(Console.ReadLine());
+            Console.Write("Enter the number:");
+            string c = (Console.ReadLine());
+            print(a, b, c);
+            Console.ReadLine();
+        }
+    }
+}
